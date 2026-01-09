c0799463bcae324b223379c071384c896cce226d
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-09 21:36:32 +0530

    Add files via upload
    1.Traversal
    2.Reverse
    3.Palindrome

---
+++ 1.py
@@ -0,0 +1,87 @@
+'''1.a="artificial intelligence"
+count=0
+for char in a:
+    if 'i' in char:
+        count+=1
+print(count)'''
+
+'''2.a ="hello world"
+b = ""
+for char in a:
+    if char in "aeiou":
+        b+=char
+print(b)'''
+
+'''3.a="Hello"
+a = "0" + a[1:]
+print(a)'''
+
+'''4.n =int(input("eenter number: "))
+rev = 0
+temp = n
+while n> 0:
+    ld = n % 10
+    rev=rev*10+ld
+    n=n//10
+if rev==temp:
+    print("it is a palindrome")
+else:
+    print("it  is not a palindrome")'''
+
+'''5.a="hello world"
+b =""
+for char in a:
+    if char in "aeiou":
+        b+="*"
+    else:
+        b+=char
+print(b)'''
+
+'''6.a="Artificial Intelligence"#Interview - logic builder[traversal]
+b=""
+for ch in range(len(a)-1,-1,-1):
+    b+=a[ch]
+print(b)'''
+
+'''7.a="Machine Learning"
+b=""
+for ch in range(len(a)-1,-1,-1):
+  b+=a[ch]
+print(b)'''
+
+'''8.a="Programming"
+print(a[::-1])'''
+
+'''a="SRM UNIVERSITY" # word reversal using indexing
+print(a[::-1])'''
+
+'''9.a="examination"
+b=""
+for char in a:
+    if char in 'aeiou':
+     print(char)
+     b+=char
+print(b)
+print(len(b))'''
+
+'''10.a=str(input("enter string: ")) #palindrome 
+b="" 
+for ch in range(len(a)-1,-1,-1):
+  b+=a[ch]
+if b==a:
+  print("it is a palindrome")
+else:
+  print("it is not a palindrome ")'''
+
+  
+
+
+
+
+
+
+
+
+
+    
+    
