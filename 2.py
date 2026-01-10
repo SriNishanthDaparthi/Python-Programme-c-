05098b6e16292129dfda090c18a39f4630636b22
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-10 10:27:27 +0530

    Add files via upload
    counting number  of words in a string and counting digits in a list

---
+++ 2.py
@@ -0,0 +1,28 @@
+'''1.a="capital of TamilNadu is Chennai" #counting number words in string
+words = a.split()
+count = 0
+for word in words:
+    count+=1
+
+print(count)'''
+
+'''2.a = [1 , 2 , 3] #counting digits in a list
+count=0
+for count in a :
+  count=count+1-1
+print(count)'''
+
+'''3.a="Artificial"
+count = 0
+for ch in a:
+    if(ch == 'i'):
+        count+=1
+print(count)'''
+
+'''4.rows = 5
+for i in range(1, rows + 1):
+    for j in range(i):
+        print("*", end="")
+    print()  # Moves to the next line after each row'''
+
+
