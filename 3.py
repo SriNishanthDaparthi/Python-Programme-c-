954a23780ed6ff9c9b4f876e9589b5dd1458507d
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-10 10:40:32 +0530

    Add files via upload
    programs based on conditional statements and loops

---
+++ 3.py
@@ -0,0 +1,96 @@
+ 
+'''num = int(input("Enter a number to check Even or Odd: "))  # even or odd 
+
+if num % 2 == 0:             
+    print("The number is Even")
+else:
+    print("The number is Odd")'''
+
+
+
+'''marks = int(input("Enter your marks: "))  #grades of student
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")'''
+
+
+
+
+
+'''print("Numbers from 1 to 10:") #printing numbers
+for i in range(1, 11):        
+    print(i)'''
+
+
+'''print("Even numbers from 1 to 20:") #printing  of even numbers
+for i in range(1, 21):        
+    if i % 2 == 0:            
+        print(i)'''
+
+
+
+
+'''print("Numbers from 1 to 5 using while loop:") #printing of numbers using while loop
+i = 1                         
+while i <= 5:                 
+    print(i)
+    i = i + 1  '''               
+
+
+
+'''i = 1                          #sum of 5 numbers
+total = 0                     
+
+while i <= 5:
+    total = total + i         
+    i = i + 1                 
+
+print("Sum of first 5 numbers is:", total)'''
+
+
+
+
+'''num = int(input("Enter a number to check Positive/Negative/Zero: ")) #nested if to chech number
+
+if num >= 0:
+    if num == 0:
+        print("Zero")
+    else:
+        print("Positive number")
+else:
+    print("Negative number")'''
+
+
+
+
+'''print("Break example:") #break in loop
+for i in range(1, 10):
+        break                  
+    if i == 5:               
+
+    print(i)'''
+
+
+
+'''print("Continue example:") # CONTINUE IN LOOP 
+
+for i in range(1, 6):
+    if i == 3:              
+        continue  
+    print(i)'''
+
+
+
+
+
+'''num = int(input("Enter a number for multiplication table: ")) #multiplication table
+
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i)'''
+
