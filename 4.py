733486fcdf2b37863cc351f4a4bdbb8101b564a4
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-11 18:40:06 +0530

    Add files via upload
    printing of patterns

---
+++ 4.py
@@ -0,0 +1,30 @@
+'''1.for r in range(1,6): #pattern printing 
+    print("*"*r)  '''   
+
+'''2.for i in range(1, 5): #number printing
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()'''
+
+'''3.for i in range(5, 0, -1): #reverse star printing
+    print("*" * i)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
