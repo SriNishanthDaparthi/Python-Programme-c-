8e12f811d37ca60124d44d438a538dc0b0220553
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-12 19:05:11 +0530

    Add files via upload
    largest, sum and positive/negative /neutral

---
+++ 5.py
@@ -0,0 +1,24 @@
+'''1.a = int(input("Enter first number: "))  #largest of two numbers
+b = int(input("Enter second number: "))
+if a > b:
+    print("Largest is:", a)
+else:
+    print("Largest is:", b)'''
+
+
+'''2.n = int(input("Enter n: "))   #sum of n numbers
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+print("Sum is:", sum)'''
+
+
+'''3.num = int(input("Enter a number: "))  #positive ,negative and neutral number
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")'''
+
+
