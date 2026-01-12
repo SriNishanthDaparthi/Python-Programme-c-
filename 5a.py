e8a58b546984f37c887d40baa8ac0599a997d262
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-12 19:06:26 +0530

    Add files via upload
    printing letters of a string, finding leap year and reverse of a number

---
+++ 5a.py
@@ -0,0 +1,24 @@
+'''1.name = input("Enter your name: ") #printing letters of a string
+for ch in name:
+    print(ch)'''
+
+
+
+'''2.year = int(input("Enter a year: ")) #finding leap year
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")'''
+
+
+'''3.num = int(input("Enter a number: "))  #reverse of a number
+rev = 0
+
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    num //= 10
+
+print("Reversed number:", rev)'''
+
+
