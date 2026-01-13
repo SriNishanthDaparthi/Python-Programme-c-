b548ce2dc55f5b455400d74f83a48c6f30eae09a
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-13 19:48:24 +0530

    Add files via upload
    swapping of numbers and fibonacci series

---
+++ 6a.py
@@ -0,0 +1,14 @@
+'''a = 5             #swapping of numbers
+b = 10
+a, b = b, a
+print("a =", a)
+print("b =", b)'''
+
+
+'''def fibonacci(n):          #fibonacci series
+    a, b = 0, 1
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+fibonacci(7)'''
+
