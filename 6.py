0f871cde75d03fbd1c7384c0c0ea05d7e5aa064f
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-13 19:47:38 +0530

    Add files via upload
    count frequency of elements in a list and finding second largest number in a list

---
+++ 6.py
@@ -0,0 +1,12 @@
+'''nums = [1, 2, 2, 3, 1, 4, 2] #Count Frequency of Elements in a List
+freq = {}
+for n in nums:
+    freq[n] = freq.get(n, 0) + 1
+print(freq)'''
+
+
+'''lst = [10, 20, 4, 45, 99]  #finding second largest number in list
+lst = list(set(lst))   
+lst.sort()
+print("Second largest:", lst[-2])'''
+
