4ee4e1380684a7ff9a8512fd8cb4d79a6f8fd5cf
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-14 21:47:31 +0530

    Add files via upload
    simple calculator and counting vowels in a string

---
+++ 7a.py
@@ -0,0 +1,29 @@
+'''1.a = int(input("Enter first number: "))  #simple calculator
+b = int(input("Enter second number: "))
+
+print("1.Add  2.Sub  3.Mul  4.Div")
+choice = int(input("Enter choice: "))
+
+if choice == 1:
+    print(a + b)
+elif choice == 2:
+    print(a - b)
+elif choice == 3:
+    print(a * b)
+elif choice == 4:
+    print(a / b)
+else:
+    print("Invalid choice")'''
+
+
+'''2.s = input("Enter a string: ")
+count = 0
+
+or ch in s:
+    if ch in "aeiouAEIOU":
+        count += 1
+print("Number of vowels:", count)'''
+
+
+
+
