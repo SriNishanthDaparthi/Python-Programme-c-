0aca8110e16a5a21b372ba1320d7db48fcf70758
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-14 21:46:45 +0530

    Add files via upload
    armstrong number and sum of digits in a number

---
+++ 7.py
@@ -0,0 +1,22 @@
+'''1.num = int(input("Enter a number: ")) #armstrong number
+temp = num
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+if sum == num:
+    print("Armstrong number")
+else:
+    print("Not an Armstrong number")'''
+
+
+'''2.num = int(input("Enter number: "))
+sum = 0
+
+while num > 0:
+    sum += num % 10
+    num //= 10
+print("Sum of digits:", sum)'''
+
