5606953851ad48923858babdc5febac140fe1d63
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-15 21:09:39 +0530

    Add files via upload
    finding ASCII values and finding common elements in list

---
+++ 8a.py
@@ -0,0 +1,12 @@
+'''1.ch = input("Enter a character: ")  #finding ASCII values
+print("ASCII value:", ord(ch))'''
+
+  
+'''2.a = [1, 2, 3, 4]  #finding common elements in list
+b = [3, 4, 5, 6]
+common = []
+for i in a:
+    if i in b:
+        common.append(i)
+print("Common elements:", common)'''
+
