16f8628c87d3284061c635c90aed6ebf392d711d
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-15 21:08:28 +0530

    Add files via upload
    removing duplicates from list and counting number of words in a list

---
+++ 8.py
@@ -0,0 +1,12 @@
+'''1.nums = [1, 2, 2, 3, 4, 4, 5]    #removing duplicates from list
+unique = []
+for n in nums:
+    if n not in unique:
+        unique.append(n)
+print(unique)'''
+
+
+'''2.sentence = input("Enter a sentence: ") #counting number of words in list
+words = sentence.split()
+
+print("Number of words:", len(words))'''
