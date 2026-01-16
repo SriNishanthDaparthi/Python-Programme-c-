1a54538d87c7e6ae9922000a584cab9fad5d88f9
SriNishanthDaparthi <srinishanthdaparthi@gmail.com>
2026-01-16 21:53:13 +0530

    Add files via upload
    pattern printing and removing duplicates from list

---
+++ 9a.py
@@ -0,0 +1,14 @@
+'''1.n = 5                               #pattern printing
+for i in range(1, n + 1):
+    print(" " * (n - i) + "* " * i)'''
+
+
+'''2.lst = list(map(int, input("Enter numbers: ").split())) #removing duplicates from list
+unique = []
+
+for i in lst:
+    if i not in unique:
+        unique.append(i)
+
+print("After removing duplicates:", unique)'''
+
